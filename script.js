b201c078de3e793394736f20733159010395c9a8
Joabson Vasconcelos <119760277+joabsonvasconcelos@users.noreply.github.com>
2023-09-11 14:16:16 -0300

    create js document
    create js document

---
+++ script.js
@@ -0,0 +1,30 @@
+function calcularIMC() {
+    var peso = parseFloat(document.getElementById("peso").value);
+    var altura = parseFloat(document.getElementById("altura").value);
+
+    if (!isNaN(peso) && !isNaN(altura) && altura > 0) {
+        var imc = peso / (altura * altura);
+        document.getElementById("resultado").textContent = imc.toFixed(2);
+
+        var classificacao = "";
+
+        if (imc < 18.5) {
+            classificacao = "Baixo peso";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            classificacao = "Peso normal";
+        } else if (imc >= 25 && imc <= 29.9) {
+            classificacao = "Sobrepeso";
+        } else if (imc >= 30 && imc <= 34.9) {
+            classificacao = "Obesidade grau I";
+        } else if (imc >= 35 && imc <= 39.9) {
+            classificacao = "Obesidade grau II";
+        } else {
+            classificacao = "Obesidade grau III";
+        }
+
+        document.getElementById("classificacao").textContent = classificacao;
+    } else {
+        document.getElementById("resultado").textContent = "Por favor, insira valores válidos.";
+        document.getElementById("classificacao").textContent = "";
+    }
+}
